8a2ccaee3e6e733027ba4a5ff28d8f466d119845
Ilya Ivanov <74549273+Ilyaivanov60@users.noreply.github.com>
2020-12-16 16:47:09 +0300

    первая версия регистрации-логин пользователя (#3)

---
+++ webapp/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm the password', validators=[DataRequired()])
+    email = StringField('Your email', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit_in = SubmitField('Log in')
+    submit_out = SubmitField('Log out')
+    confirm = SubmitField('confirm')
+    
