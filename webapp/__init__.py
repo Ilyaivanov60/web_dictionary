9f1a09f1b7227fedff4cad1e509fdca3f24bc20b
ILya Ivanov <ivanovrocket@gmail.com>
2021-01-28 13:49:07 +0300

    добавил blueprint для user

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, redirect, render_template, flash, url_for
-from flask_login import LoginManager, current_user, login_user, logout_user
+from flask_login import LoginManager, current_user
 from flask_migrate import Migrate
 
 from get_translated_word import get_translation
 from webapp.db import db
-from webapp.model import User, Cards
-from webapp.forms import LoginForm, RegistrationForm, WordForm
+from webapp.model import Cards
+from webapp.user.models import User
+from webapp.forms import WordForm
+from webapp.user.views import blueprint as user_blueprint
 
 
 def create_app():
@@ -16,7 +18,9 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'user.login'
+
+    app.register_blueprint(user_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -37,7 +41,7 @@ def create_app():
             return render_template('my_dictionary.html', page_title=title, cards=cards_db, form=word_form)
         else:
             flash('вы не вошли на сайт')
-            return redirect(url_for('login'))
+            return redirect(url_for('user.login'))
 
     @app.route('/add_word', methods=["POST"])
     def add_word():
@@ -92,62 +96,5 @@ def create_app():
 
             else:
                 return redirect(url_for('my_dictionary'))
-
-    @app.route('/login')
-    def login():
-        if current_user.is_authenticated:
-            return redirect(url_for('index'))
-        form = LoginForm()
-        title = 'Авторизация'
-        return render_template('login.html', page_title=title, form=form)
-
-    @app.route('/process_login', methods=['POST'])
-    def process_login():
-        form = LoginForm()
-        if form.validate_on_submit():
-            user = User.query.filter(User.username == form.username.data).first()
-            if user and user.check_password(form.password.data):
-                login_user(user)
-                flash('вы успешно вошли на сайт')
-                return redirect(url_for('index'))
-
-        flash('неправильный имя или пароль')
-        return redirect(url_for('login'))
-
-    @app.route('/logout')
-    def logout():
-        logout_user()
-        flash('Вы успешно разлогины')
-        return redirect(url_for('index'))
-
-
-
-    @app.route('/registration')
-    def registration():
-        if current_user.is_authenticated:
-            return redirect(url_for('index'))
-        else:
-            form_reg = RegistrationForm()
-            title = "Регистрация"
-            return render_template('registration.html', page_title=title, form=form_reg)
-
-    @app.route('/process_reg', methods=['POST'])
-    def process_reg():
-        form = RegistrationForm()
-        if form.validate_on_submit():
-            new_user = User(username=form.username.data, email=form.email.data)
-            new_user.set_password(form.password.data)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Вы успешно зарегистрировались!')
-            return redirect(url_for('index'))
-        else:
-            for field, errors in form.errors.items():
-                for error in errors:
-                    flash('Ошибка в поле {}: - {}'.format(
-                        getattr(form, field).label.text,
-                        error
-                    ))
-            return redirect(url_for('registration'))
 
     return app
--- webapp/forms.py
+++ webapp/forms.py
@@ -1,14 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
-from webapp.model import User
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
-    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form-control"})
-    remember_me = BooleanField('Запомнить меня', default=True,  render_kw={"class": "form-check-input"})
-    submit = SubmitField('Отправить!', render_kw={"class": "btn btn-primary"})
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class WordForm(FlaskForm):
@@ -18,21 +10,3 @@ class WordForm(FlaskForm):
     add = SubmitField('Добавить', render_kw={"class": "btn btn-primary"})
     edit = SubmitField('Редактировать', render_kw={"class": "btn btn-primary"})
     delete = SubmitField('Удалить', render_kw={"class": "btn btn-primary"})
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
-    email = StringField('Электронная почта', validators=[DataRequired(), Email()], render_kw={"class": "form-control"})
-    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form-control"})
-    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')], render_kw={"class": "form-control"})
-    submit = SubmitField('Зарегистрироваться', render_kw={"class": "btn btn-primary"})
-
-    def validate_username(self, username):
-        users_count = User.query.filter_by(username=username.data).count()
-        if users_count > 0:
-            raise ValidationError('Пользователь с таким именем уже зарегистрирован')
-
-    def validate_email(self, email):
-        users_count = User.query.filter_by(email=email.data).count()
-        if users_count > 0:
-            raise ValidationError('Пользователь с такой электронной почтой уже зарегистрирован')
